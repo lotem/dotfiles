d08f64b7435af05c21b17a33b96a1af24a321744
Chen Gong <chen.sst@gmail.com>
2018-11-02 12:17:12 +0800

    fix(dot/emacs.d): adapt to emacs prelude

---
+++ dot/emacs.d/personal/init-personal.el
@@ -0,0 +1,9 @@
+(prelude-require-package 'snazzy-theme)
+(load-theme 'snazzy t)
+
+;; set the default font
+(set-face-attribute 'default nil
+                    :family "Iosevka"
+                    :slant 'normal
+                    :weight 'light
+                    :height 180)

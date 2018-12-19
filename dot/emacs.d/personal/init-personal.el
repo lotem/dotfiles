0c194be2748ca5d2b4286747090ecfdef6606bbf
Chen Gong <chen.sst@gmail.com>
2018-12-19 11:48:54 +0800

    feat(emacs): switch to github theme

--- dot/emacs.d/personal/init-personal.el
+++ dot/emacs.d/personal/init-personal.el
@@ -1,5 +1,6 @@
+(prelude-require-package 'github-theme)
 (prelude-require-package 'snazzy-theme)
-(load-theme 'snazzy t)
+(load-theme 'github t)
 
 ;; set the default font
 (set-face-attribute 'default nil

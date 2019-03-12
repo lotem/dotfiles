6aa8dfd34a5f6418dee111493e1dd12d8357929c
Chen Gong <chen.sst@gmail.com>
2019-03-12 21:00:40 +0800

    feat(emacs): code for loading different themes for terminal and gui

--- dot/emacs.d/personal/init-personal.el
+++ dot/emacs.d/personal/init-personal.el
@@ -1,6 +1,24 @@
 (prelude-require-package 'github-theme)
 (prelude-require-package 'snazzy-theme)
-(load-theme 'github t)
+;; last t is for NO-ENABLE
+;; (load-theme 'github t t)
+;; (load-theme 'snazzy t t)
+
+;; (defun mb/pick-color-theme (frame)
+;;   (select-frame frame)
+;;   (if (window-system frame)
+;;       (progn
+;;         (disable-theme 'snazzy) ; in case it was active
+;;         (enable-theme 'github))
+;;     (progn
+;;       (disable-theme 'github) ; in case it was active
+;;       (enable-theme 'snazzy))))
+;; (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+
+;; For when started with emacs or emacs -nw rather than emacs --daemon
+;; (if window-system
+;;     (enable-theme 'github)
+;;   (enable-theme 'snazzy))
 
 ;; set the default font
 (set-face-attribute 'default nil

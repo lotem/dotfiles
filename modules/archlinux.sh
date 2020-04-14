746c877836c4590febf7993b37c1310dc02c097c
Chen Gong <chen.sst@gmail.com>
2020-04-14 22:25:46 +0800

    (archlinux): install yay as AUR helper

--- modules/archlinux.sh
+++ modules/archlinux.sh
@@ -19,7 +19,7 @@ pacman_install() {
 }
 
 install_aur_packages() {
-  install_packer
+  install_yay
   if [ ${#aur_packages[@]} -ne 0 ]; then
     fancy_echo 'Installling AUR packages ...'
     map aur_install "${aur_packages[@]}"
@@ -30,23 +30,23 @@ aur_install() {
   if pacman -Qkq $@ &> /dev/null; then
     fancy_echo "Package '%s' is already installed. Skipping ..." $@
   else
-    packer -S --noedit "$@"
+    yay -S --noedit "$@"
   fi
 }
 
 aur_package_url='https://aur.archlinux.org/packages/%s'
 aur_snapshot_url='https://aur.archlinux.org/cgit/aur.git/snapshot/%s.tar.gz'
-packer_build_dir='/tmp/packer-build'
+yay_build_dir='/tmp/yay-build'
 
-install_packer() {
-  if ! command -v packer &> /dev/null; then
+install_yay() {
+  if ! command -v yay &> /dev/null; then
     pacman_install 'base-devel'
-    fancy_echo "Installing packer from ${aur_package_url}" packer
-    mkdir -p "${packer_build_dir}"
-    pushd "${packer_build_dir}" &>/dev/null
-    curl -L -O $(printf "${aur_snapshot_url}" packer)
-    tar -xvf packer.tar.gz
-    cd packer
+    fancy_echo "Installing yay from ${aur_package_url}" yay
+    mkdir -p "${yay_build_dir}"
+    pushd "${yay_build_dir}" &>/dev/null
+    curl -L -O $(printf "${aur_snapshot_url}" yay)
+    tar -xvf yay.tar.gz
+    cd yay
     makepkg -si
     popd &>/dev/null
   fi

adbbee2ec3c337c4074f77e51b9c8587f63a46d5
Chen Gong <chen.sst@gmail.com>
2020-12-28 23:05:34 +0800

    refactor(.emacs.d): init theme in a separate module

--- dot/emacs.d/personal/init-personal.el
+++ dot/emacs.d/personal/init-personal.el
@@ -6,33 +6,6 @@
 
 (prelude-require-package 'ripgrep)
 
-(prelude-require-package 'almost-mono-themes)
-;; (prelude-require-package 'github-theme)
-;; (prelude-require-package 'leuven-theme)
-;; (prelude-require-package 'minimal-theme)
-;; (prelude-require-package 'monokai-theme)
-;; (prelude-require-package 'snazzy-theme)
-
-;; last t is for NO-ENABLE
-(load-theme 'almost-mono-black t t)
-(load-theme 'almost-mono-white t t)
-
-(defun mb/pick-color-theme (frame)
-  (select-frame frame)
-  (if (window-system frame)
-      (progn
-        (disable-theme 'almost-mono-black) ; in case it was active
-        (enable-theme 'almost-mono-white))
-    (progn
-      (disable-theme 'almost-mono-white) ; in case it was active
-      (enable-theme 'almost-mono-black))))
-(add-hook 'after-make-frame-functions 'mb/pick-color-theme)
-
-;; For when started with emacs or emacs -nw rather than emacs --daemon
-(if window-system
-    (enable-theme 'almost-mono-white)
-  (enable-theme 'almost-mono-black))
-
 ;; set the default font
 (set-face-attribute 'default nil
                     :family "Iosevka Slab"
---
+++ dot/emacs.d/personal/init-theme.el
@@ -0,0 +1,29 @@
+(defun pick-color-theme (window-theme terminal-theme)
+  ;; last t is for NO-ENABLE
+  (load-theme window-theme t t)
+  (load-theme terminal-theme t t)
+
+  (defun mb/pick-color-theme (frame)
+    (select-frame frame)
+    (if (window-system frame)
+        (progn
+          (disable-theme terminal-theme) ; in case it was active
+          (enable-theme window-theme))
+      (progn
+        (disable-theme window-theme) ; in case it was active
+        (enable-theme terminal-theme))))
+  (add-hook 'after-make-frame-functions 'mb/pick-color-theme)
+
+  ;; For when started with emacs or emacs -nw rather than emacs --daemon
+  (if window-system
+      (enable-theme window-theme)
+    (enable-theme terminal-theme)))
+
+(prelude-require-package 'almost-mono-themes)
+;; (prelude-require-package 'github-theme)
+;; (prelude-require-package 'leuven-theme)
+;; (prelude-require-package 'minimal-theme)
+;; (prelude-require-package 'monokai-theme)
+;; (prelude-require-package 'snazzy-theme)
+
+(pick-color-theme 'almost-mono-white 'almost-mono-black)

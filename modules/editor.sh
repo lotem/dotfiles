3da63b2a83333dc166ac30da53cbc9545783f1b7
居戎氏 <chen.sst@gmail.com>
2022-04-13 12:18:55 +0800

    use official centaur emacs

--- modules/editor.sh
+++ modules/editor.sh
@@ -6,8 +6,6 @@ setup_emacs() {
   if [ ! -d "$HOME/.emacs.d" ]; then
     if [ "$MY_EMACS_CONFIG" = 'centaur' ]; then
       git_clone_or_pull 'https://github.com/seagle0128/.emacs.d.git' "$HOME/.emacs.d" --depth 1
-    elif [ "$MY_EMACS_CONFIG" = 'lotem/centaur-emacs' ]; then
-      git_clone_or_pull 'https://github.com/lotem/centaur-emacs' "$HOME/.emacs.d" --depth 1
     elif [ "$MY_EMACS_CONFIG" = 'prelude' ]; then
       curl -L https://git.io/epre | sh
     elif [ "$MY_EMACS_CONFIG" = 'purcell' ]; then
